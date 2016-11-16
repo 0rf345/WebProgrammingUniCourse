e86d79178334e4593026c1003d88e79e078ead07
Orfeas Kalipolitis <csd3285@csd.uoc.gr>
2016-11-16 20:29:15 +0200

    Registration form in place working on REGEXes

--- a3/NBproject/web/js/javascript.js
+++ a3/NBproject/web/js/javascript.js
@@ -1,10 +1,43 @@
+// These help with coding the page
+var p = "<p>";
+var ep= "</p>";
+var l = "<label>";
+var el= "</label>";
+
+function validatePassword(){
+  
+  if($("#userp").valid()) {
+      $("#userpp").pattern = $("#userp").pattern;
+  }
+  
+}
+
 function loginFormCreate() {
-    var usern = "<input type='text' id='usern' value='username' />";
-    var userp = "<input type='password' id='userp' value='password' />";
-    $("#loginForm").html("");
-    $("#loginForm").append("<p>Username "+usern+"</p>");
-    $("#loginForm").append("<p>Password "+userp+"</p>");
-    $("#loginForm").append("<p><input type='submit' value='Login' onclick='loginPOST();' /></p>");
+    $("#form").html("");
+    $("#form").append(p+l+"Username"+el+"<input type='text' id='usern' placeholder='username' />"+ep);
+    $("#form").append(p+l+"Password"+el+"<input type='password' id='userp' placeholder='password' />"+ep);
+    $("#form").append("<p><input type='submit' value='Login' onclick='loginPOST();' /></p>");
+}
+
+function registerFormCreate() {
+    $("#form").html("");
+    $("#form").append(p+l+"Username"+el+"<input type='text' id='usern' placeholder='username' pattern='[a-zA-Z0-9]{8,}' required title='8 or more characters' />"+ep);
+    $("#form").append(p+l+"e-mail"+el+"<input type='email' id='email' placeholder='johndoe@gmail.com' pattern='[a-z0-9._%+-]+@[a-z0-9.-]+[.]+\.[a-z]{0,}$' required title='text(@)text(.)text Can have more (.)' />"+ep);
+    $("#form").append(p+l+"Password"+el+"<input type='password' id='userp' placeholder='password' required title='6-10 characters, must contain 1 latin character, a number and a special symbol'/>"+ep);
+    $("#form").append(p+l+"Confirm Password"+el+"<input type='password' id='userpp' placeholder='password' required />"+ep);
+    $("#form").append(p+l+"FirstName"+el+"<input type='text' id='fname' placeholder='John' />"+ep);
+    $("#form").append(p+l+"LastName"+el+"<input type='password' id='lname' placeholder='Doe' />"+ep);
+    $("#form").append(p+l+"Birthday"+el+"<input type='date' id='bdate' max='2001-01-01'/>"+ep); // Going with year alone on defining age. The UI format is based on the user's locale.
+    $("#form").append(p+"<input type='radio' name='sex' value='1' />Not applicable"+ep);
+    $("#form").append(p+"<input type='radio' name='sex' value='2'>Male"+ep);
+    $("#form").append(p+"<input type='radio' name='sex' value='3'>Female"+ep);
+    $("#form").append(p+l+"Town"+el+"<input type='text' id='town' placeholder='Honolulu' />"+ep);
+    $("#form").append(p+"<input type='submit' value='Register' onclick='registerPOST();' />"+ep);
+    $("#userp").on("change", validatePassword);
+    
+    
+    password = $("#userp");
+    confirm_password = $("#userpp");
 }
 
 function loginPOST() {
@@ -25,3 +58,7 @@ function loginPOST() {
     xhr.setRequestHeader('ContentType','application/x-www-form-urlencoded');
     xhr.send();
 }   
+
+function registerPOST() {
+    
+}
